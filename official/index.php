3648a775a06e51ca7e5162762969e8108b0ac7e6
Anlimah <y.m.ratty7@gmail.com>
2023-12-11 11:58:49 +0000

    completing setup

---
+++ official/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>official home Dashboard</title>
+
+
+
+    <link rel="stylesheet" type="text/css" href="style 1.css">
+</head>
+
+<body>
+    <header>
+        <h1>official home Dashboard</h1>
+
+
+        <button class="report a case">report a case</button>
+        </section>
+        <section>
+            <button class="View reported cases">view reported cases</button>
+        </section>
+        <section>
+
+        </section>
+        <footer>
+            <p>© 2023 Your Company. All rights reserved.</p>
+        </footer>
+</body>
+
+</html>
